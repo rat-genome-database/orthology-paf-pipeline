8653902307643f18840e1feb0795bfa1124067ef
jdepons <jdepons@mcw.edu>
2024-08-28 13:49:07 -0500

    updated

--- src/main/dist/runAll.sh
+++ src/main/dist/runAll.sh
@@ -10,6 +10,22 @@ if [ "$SERVER" == "REED" ]; then
 fi
 
 # run for all species in RGD, except human (transitive orthologs are made between human and given species)
-$APPDIR/run.sh "0" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "GRCh38.p14" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "GRCh37.p13" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "NCBI36" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "GRCm39" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "GRCm38" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "MGSCv37" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "CanFam3.1" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "Sscrofa11.1" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+
+
+$APPDIR/run.sh "mRatBN7.2" 372 "GRCh38.p14" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "GRCh38.p14" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "GRCh38.p14" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+$APPDIR/run.sh "mRatBN7.2" 372 "GRCh38.p14" 38 "/tmp/test" 2>&1 > $APPDIR/run.log
+
+
+
 
 mailx -s "[$SERVER] orthology PAF for all jbrowse assemblies" $EMAILLIST < $APPDIR/logs/summary.log
--- src/main/java/edu/mcw/rgd/OrthologyPafPipeline/Manager.java
+++ src/main/java/edu/mcw/rgd/OrthologyPafPipeline/Manager.java
@@ -7,6 +7,7 @@ import edu.mcw.rgd.datamodel.MappedOrtholog;
 import edu.mcw.rgd.datamodel.Ortholog;
 import edu.mcw.rgd.datamodel.SpeciesType;
 import edu.mcw.rgd.process.Utils;
+import edu.mcw.rgd.process.mapping.MapManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -15,10 +16,7 @@ import org.springframework.core.io.FileSystemResource;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
@@ -46,6 +44,32 @@ public class Manager {
      */
     public static void main(String[] args) throws Exception {
 
+
+
+
+        LinkedHashMap<String, Integer> assemblies = new LinkedHashMap<String, Integer>();
+        assemblies.put("mRatBN7.2",372);
+        assemblies.put("Rnor_6.0",360);
+        assemblies.put("Rnor_5.0",70);
+        assemblies.put("RGSC_v3.4",60);
+        assemblies.put("GRCh38.p14",38);
+        assemblies.put("GRCh37.p13",17);
+        assemblies.put("NCBI36",13);
+        assemblies.put("GRCm39",239);
+        assemblies.put("GRCm38",35);
+        assemblies.put("MGSCv37",18);
+        assemblies.put("CanFam3.1",631);
+        assemblies.put("Sscrofa11.1",911);
+        assemblies.put("Sscrofa10.2",910);
+        assemblies.put("Chlorocebus_sabeus 1.1",1311);
+        assemblies.put("Vero_WHO_p1.0",1313);
+        assemblies.put("Mhudiblu_PPA_v0",513);
+        assemblies.put("PanPan1.1",511);
+        assemblies.put("HetGla_female_1.0",1410);
+        assemblies.put("ChiLan1.0",44);
+        assemblies.put("SpeTri2.0",720);
+
+
         DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
         new XmlBeanDefinitionReader(bf).loadBeanDefinitions(new FileSystemResource("properties/AppConfigure.xml"));
         Manager manager = (Manager) bf.getBean("main");
@@ -68,8 +92,35 @@ public class Manager {
 
         Date time0 = Calendar.getInstance().getTime();
 
+        HashMap<Integer,Boolean> processed = new HashMap<Integer,Boolean>();
+
         try {
-                manager.run(assembly1, mapKey1, assembly2, mapKey2, outputDirectory);
+            for (Map.Entry<String, Integer> entry: assemblies.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+
+                for (Map.Entry<String,Integer> entry2: assemblies.entrySet()) {
+
+                    String key2 = entry2.getKey();
+                    Integer value2 = entry2.getValue();
+
+                    //if (!key.equals(key2)) {
+                    if (MapManager.getInstance().getMap(value).getSpeciesTypeKey() != MapManager.getInstance().getMap(value2).getSpeciesTypeKey()) {
+
+                        if (!processed.containsKey(value2)) {
+                            manager.run(key, value, key2, value2, outputDirectory);
+                        }
+                    }
+
+                }
+                processed.put(value,true);
+
+//                assemblies.remove(key);
+
+            }
+
+
+               // manager.run(assembly1, mapKey1, assembly2, mapKey2, outputDirectory);
         } catch(Exception e) {
             Utils.printStackTrace(e, manager.logger);
             throw e;
@@ -104,7 +155,8 @@ public class Manager {
         Map<String,Integer> chrLen2 = mdao.getChromosomeSizes(mapKey2);
 
         OrthologDAO odao = new OrthologDAO();
-        List<MappedOrtholog> ortho = odao.getAllMappedOrthologs(3,1,mapKey1,mapKey2);
+
+        List<MappedOrtholog> ortho = odao.getAllMappedOrthologs(MapManager.getInstance().getMap(mapKey1).getSpeciesTypeKey(),MapManager.getInstance().getMap(mapKey2).getSpeciesTypeKey(),mapKey1,mapKey2);
 
         FileWriter fw = new FileWriter(new File(outputDirectory + "/" + assembly1 +".bed"));
         for (MappedOrtholog mo: ortho) {
@@ -140,6 +192,66 @@ public class Manager {
             row += "100\t";
             fw.write(row + "\n");
         }
+        fw.close();
+
+String orthologyTemplate = """
+        {
+            "type":"SyntenyTrack",
+                "trackId":"assembly12assembly2.anchors",
+                "name":"assembly1-assembly2 orthology",
+                "assemblyNames": [
+                    "assembly1",
+                    "assembly2"
+            ],
+            "category": [
+                "Orthologs"
+            ],
+            "adapter":{
+            "type":"MCScanAnchorsAdapter",
+                    "mcscanAnchorsLocation":{
+                    "locationType":"UriLocation",
+                    "uri":"assembly1-assembly2.anchors"
+            },
+            "bed1Location":{
+                "locationType":"UriLocation",
+                        "uri":"assembly1/assembly1.bed"
+            },
+            "bed2Location":{
+                "locationType":"UriLocation",
+                        "uri":"assembly1/assembly2.bed"
+            },
+            "assemblyNames": [
+                "assembly1",
+                "assembly2"
+            ]
+            },
+            "displays": [
+            {
+                "type":"DotplotDisplay",
+                    "displayId":"assembly12assmbly2.anchors-DotplotDisplay"
+            },
+            {
+                "type":"LinearComparativeDisplay",
+                    "displayId":"assembly12assembly2.anchors-LinearComparativeDisplay"
+            },
+            {
+                "type":"LinearSyntenyDisplay",
+                    "displayId":"assembly12assembly2.anchors-LinearSyntenyDisplay"
+            },
+            {
+                "type":"LGVSyntenyDisplay",
+                    "displayId":"assembly12assembly2.anchors-LGVSyntenyDisplay",
+                    "mouseover":"jexl:get(feature,'mate').name"
+            }
+        ]
+        }
+
+""";
+
+    orthologyTemplate = orthologyTemplate.replaceAll("assembly1",assembly1);
+        orthologyTemplate = orthologyTemplate.replaceAll("assembly2",assembly2);
+        fw = new FileWriter(new File(outputDirectory + "/" + assembly1 + "-" + assembly2 +".json"));
+        fw.write(orthologyTemplate);
         fw.close();
 
     }
